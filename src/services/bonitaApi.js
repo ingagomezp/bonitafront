fec960ce27ff9e320f06ec84943e990eb9e06c0f
Andres Felipe Gomez Parada <andres.gomez@bougo.com.co>
2022-04-04 12:33:27 -0500

    ajuste general

--- src/services/bonitaApi.js
+++ src/services/bonitaApi.js
@@ -7,15 +7,18 @@ const headers = {
 
 export const getDataContractPurchaseOrder = async (idcase) => {
     const response = await axios.get('http://188.166.68.70:3004/api/v1/task/getContractPurchaseOrder/' + idcase, { headers: headers });
+    console.log('getDataContractPurchaseOrder', response.data)
     return response.data.body
 };
 
 export const getDataContractLead = async (idcase) => {
     const response = await axios.get('http://188.166.68.70:3004/api/v1/task/getContractLead/' + idcase, { headers: headers });
+    console.log('getDataContractLead', response.data)
     return response.data.body
 };
 
 export const postExecuteTask = async (contracts, idcase) => {
     const response = await axios.post('http://188.166.68.70:3004/api/v1/task/executewithallcontractbyidcase/' + idcase, { contracts }, { headers: headers });
+    console.log('postExecuteTask', response.data)
     return response.data.body
 };
